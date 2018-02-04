fb8b54325e20cb226a0247321ad2211fcd75c62f
Jonathan Marsh <j.r.marsh@ntlworld.com>
2018-02-04 01:32:27 +0000

    Basic jump

--- index.js
+++ index.js
@@ -17,6 +17,8 @@ window.addEventListener('load', function() {
 	addImage('rocks.png');			//6
 	addImage('forest4.png');		//7
 	addImage('totem1.png');			//8
+	addImage('totem2.png');			//9
+	addImage('totem3.png');			//10
 	
 	var tick = 0;
 	class object{
@@ -33,8 +35,6 @@ window.addEventListener('load', function() {
 	object.prototype.draw = function(){
 		context.fillStyle = this.fill;
 		var offset = (tick - this.t) * this.depth;
-		if(this.x - offset + this.w < 0) 
-			this.t = tick;
 		context.drawImage(this.fill, this.x - offset, this.y, this.w, this.h);
 	}
 	class background extends object{
@@ -44,36 +44,66 @@ window.addEventListener('load', function() {
 	}
 	background.prototype.draw = function(){
 		var offset = (tick - this.t) * this.depth;
-		if(this.x - offset + canvas.width < 0) 
-			this.t = tick;
 		
 		var pattern = context.createPattern(this.fill, 'repeat');
 		context.fillStyle = pattern;
 		context.translate(-offset, 0);
 		context.fillRect(0,0,canvas.width*2,canvas.height);
 		context.translate(offset,0);
+		
+		if(this.x - offset + canvas.width < 0) 
+			this.t = tick;
 	}
-	var objects = [];
-	objects.push(new background(1, 4));		//mountain
-	objects.push(new background(2, 3));		//grass
-	objects.push(new background(7, 2.9));	//forest4
-	objects.push(new background(3, 2.5));	//forest3
-	objects.push(new background(6, 2.25));	//rocks
-	objects.push(new background(5, 2));		//forest1
-	objects.push(new background(4, 2));		//floor
+	var backgrounds = [];
+	backgrounds.push(new background(1, 4));		//mountain
+	backgrounds.push(new background(2, 3));		//grass
+	backgrounds.push(new background(7, 2.9));	//forest4
+	backgrounds.push(new background(3, 2.5));	//forest3
+	backgrounds.push(new background(6, 2.25));	//rocks
+	backgrounds.push(new background(5, 2));		//forest1
+	backgrounds.push(new background(4, 2));		//floor
 	
-	objects.push(new object(imgs[8], canvas.width, 0, 1, 59, canvas.height))
-	
-	var player = new object(imgs[0], canvas.width/2 - 25, canvas.height/2 - 25, 0, 50, 50);
-	
+	var objects = [],
+		player = new object(imgs[0], canvas.width/2 - 25, canvas.height/2 - 25, 0, 50, 50),
+		velocity = 0,
+		gravity = 0.5,
+		jump = -10,
+		alive = true;
+	objects.push(new object(imgs[8], canvas.width, 0, 2.5, 59, canvas.height));
+	canvas.onclick = function (event){
+		velocity = jump;
+	}
+	canvas.oncontextmenu = function (e) {
+		e.preventDefault();
+	};
 	(function draw() {
 		requestAnimationFrame(draw);
 		context.clearRect(0, 0, canvas.width, canvas.height);
 		tick++;
 		
-		for(var item in objects)
-			objects[item].draw();
-		
+		if(document.activeElement === canvas && alive){
+			velocity += gravity;
+			player.y += velocity;
+		}
+		for(var item in backgrounds)
+			backgrounds[item].draw();
+		for(var item in objects){
+			var offset = (tick - objects[item].t) * objects[item].depth;
+			if(objects[item].x - offset + objects[item].w < 0)
+				delete objects[item];
+			else{ 
+				console.log(objects[item].x - offset);
+				if(objects[item].x - offset - objects[item].w/2 <= canvas.width/2 && !objects[item].spawned && alive){
+					objects.push(new object(imgs[8+ Math.floor(Math.random()*3)], canvas.width, 0, 2.5, 59, canvas.height))
+					objects[item].spawned = true;
+				}
+				objects[item].draw();
+			}
+		}
+		if(player.y + velocity < 0)
+			velocity = gravity;
+		if(player.y + player.h >= canvas.height-68)
+			alive = false;
 		player.draw();
 	})();
 }); 
