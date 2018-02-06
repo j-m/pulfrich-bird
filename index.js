f287ffe21b06b38906f1cddf40ef4a68b2fe31ef
James Vickery <dev@jamesvickery.net>
2018-02-06 19:15:55 +0000

    Spacebar support yay

--- index.js
+++ index.js
@@ -1,30 +1,37 @@
-window.addEventListener('load', function() {
+window.addEventListener('load', function () {
 	var canvas = document.getElementsByTagName("canvas")[0],
 		context = canvas.getContext('2d');
-		
+
+	document.body.onkeyup = function (e) {
+		if (e.keyCode == 32) {
+			uhhJump();
+		}
+	}
+
 	var imgs = [];
-	function addImage(src){
+
+	function addImage(src) {
 		var tmp = new Image();
 		tmp.src = src;
 		imgs.push(tmp);
 	}
-	addImage('bird.png');			//0
-	addImage('mountainnew.png');	//1
-	addImage('grass2.png');			//2
-	addImage('forest3.png');		//3	
-	addImage('forest1.png');		//4
-	addImage('floor.png');			//5
-	addImage('rocks.png');			//6
-	addImage('forest4.png');		//7
-	addImage('totem2.png');			//8
-	addImage('totem1.png');			//9
-	addImage('totem3.png');			//10
-	addImage('retry1.png');			//11
-	addImage('retry2.png');			//12
-	
+	addImage('bird.png'); //0
+	addImage('mountainnew.png'); //1
+	addImage('grass2.png'); //2
+	addImage('forest3.png'); //3	
+	addImage('forest1.png'); //4
+	addImage('floor.png'); //5
+	addImage('rocks.png'); //6
+	addImage('forest4.png'); //7
+	addImage('totem2.png'); //8
+	addImage('totem1.png'); //9
+	addImage('totem3.png'); //10
+	addImage('retry1.png'); //11
+	addImage('retry2.png'); //12
+
 	var tick = 0;
-	class object{
-		constructor(fill, x, y, depth, w, h){
+	class object {
+		constructor(fill, x, y, depth, w, h) {
 			this.fill = fill;
 			this.x = x;
 			this.y = y;
@@ -34,63 +41,72 @@ window.addEventListener('load', function() {
 			this.t = tick;
 		}
 	}
-	object.prototype.draw = function(){
+	object.prototype.draw = function () {
 		context.fillStyle = this.fill;
 		var offset = (tick - this.t) * this.depth;
 		context.drawImage(this.fill, this.x - offset, this.y, this.w, this.h);
 	}
-	class background extends object{
-		constructor(index, depth){
-			super(imgs[index],0,0,depth,0,0);
+	class background extends object {
+		constructor(index, depth) {
+			super(imgs[index], 0, 0, depth, 0, 0);
 		}
 	}
-	background.prototype.draw = function(){
+	background.prototype.draw = function () {
 		var offset = (tick - this.t) * this.depth;
-		
+
 		var pattern = context.createPattern(this.fill, 'repeat');
 		context.fillStyle = pattern;
 		context.translate(-offset, 0);
-		context.fillRect(0,0,canvas.width*2,canvas.height);
-		context.translate(offset,0);
-		
-		if(this.x - offset + canvas.width < 0) 
+		context.fillRect(0, 0, canvas.width * 2, canvas.height);
+		context.translate(offset, 0);
+
+		if (this.x - offset + canvas.width < 0)
 			this.t = tick;
 	}
 	var backgrounds = [];
-	backgrounds.push(new background(1, 4));		//mountain
-	backgrounds.push(new background(2, 3));		//grass
-	backgrounds.push(new background(7, 2.9));	//forest4
-	backgrounds.push(new background(3, 2.5));	//forest3
-	backgrounds.push(new background(6, 2.25));	//rocks
-	backgrounds.push(new background(5, 2));		//forest1
-	backgrounds.push(new background(4, 2));		//floor
-	
+	backgrounds.push(new background(1, 4)); //mountain
+	backgrounds.push(new background(2, 3)); //grass
+	backgrounds.push(new background(7, 2.9)); //forest4
+	backgrounds.push(new background(3, 2.5)); //forest3
+	backgrounds.push(new background(6, 2.25)); //rocks
+	backgrounds.push(new background(5, 2)); //forest1
+	backgrounds.push(new background(4, 2)); //floor
+
 	var objects = [],
-		player = new object(imgs[0], canvas.width/2 - 30, canvas.height/2 - 25, 0, 60, 50),
+		player = new object(imgs[0], canvas.width / 2 - 30, canvas.height / 2 - 25, 0, 60, 50),
 		velocity = 0,
 		gravity = 0.5,
 		jump = -8,
 		alive = true;
 	var score = last = best = 0;
-	canvas.onclick = function (event){
+
+	function uhhJump() {
+		console.log("jumped");
 		velocity = jump;
-		if(!alive && within){
+		if (!alive && within) {
 			objects.length = 0;
-			player.y = canvas.height/2 - 30;
+			player.y = canvas.height / 2 - 30;
 			var tmp = document.createElement("input");
 			document.body.appendChild(tmp);
 			tmp.focus();
 			document.body.removeChild(tmp);
 			alive = true;
 		}
+
+	}
+
+	canvas.onclick = function (event) {
+		uhhJump();
 	}
 	canvas.oncontextmenu = function (e) {
 		e.preventDefault();
 	};
-	function died(){
+
+	function died() {
 		alive = false;
 		objects.length = 0;
-		var x = canvas.width/2-120, y = canvas.height/2-72;
+		var x = canvas.width / 2 - 120,
+			y = canvas.height / 2 - 72;
 		objects[0] = new object(imgs[11], x, y, 0, 240, 144);
 		objects[1] = new object(imgs[12], x, y, 0, 240, 144);
 		last = score;
@@ -98,47 +114,50 @@ window.addEventListener('load', function() {
 			best = last;
 		score = 0;
 	}
+
 	function getMousePos(canvas, evt) {
 		var rect = canvas.getBoundingClientRect();
 		return {
-		  x: evt.clientX - rect.left,
-		  y: evt.clientY - rect.top
+			x: evt.clientX - rect.left,
+			y: evt.clientY - rect.top
 		};
 	}
-	var within = false, next = 0;
-	canvas.addEventListener('mousemove', function(evt) {
-		var x = canvas.width/2-120, y = canvas.height/2-72,
+	var within = false,
+		next = 0;
+	canvas.addEventListener('mousemove', function (evt) {
+		var x = canvas.width / 2 - 120,
+			y = canvas.height / 2 - 72,
 			mousePos = getMousePos(canvas, evt);
-		if(!alive && mousePos.x > x && mousePos.x < x + 200 && mousePos.y > y && mousePos.y < y + 120){	
+		if (!alive && mousePos.x > x && mousePos.x < x + 200 && mousePos.y > y && mousePos.y < y + 120) {
 			canvas.style.cursor = "pointer";
 			within = true;
-		}else{
+		} else {
 			canvas.style.cursor = "default";
 			within = false;
 		}
-	}, false);	
+	}, false);
 	(function draw() {
 		requestAnimationFrame(draw);
 		context.clearRect(0, 0, canvas.width, canvas.height);
 		tick++;
-		
-		if(document.activeElement === canvas && alive){
+
+		if (document.activeElement === canvas && alive) {
 			velocity += gravity;
 			player.y += velocity;
-			if(objects.length < 1){
+			if (objects.length < 1) {
 				objects.push(new object(imgs[8], canvas.width, 0, 2.5, 59, canvas.height));
 				next = 0;
 			}
 		}
-		for(var item in backgrounds)
+		for (var item in backgrounds)
 			backgrounds[item].draw();
-		for(var item in objects){
+		for (var item in objects) {
 			var offset = (tick - objects[item].t) * objects[item].depth;
-			if(objects[item].x - offset + objects[item].w < 0)
+			if (objects[item].x - offset + objects[item].w < 0)
 				delete objects[item];
-			else{ 
-				if(objects[item].x - offset + objects[item].w <= canvas.width/2 && !objects[item].spawned && alive){
-					objects.push(new object(imgs[8+ Math.floor(Math.random()*3)], canvas.width, 0, 2.5, 59, canvas.height))
+			else {
+				if (objects[item].x - offset + objects[item].w <= canvas.width / 2 && !objects[item].spawned && alive) {
+					objects.push(new object(imgs[8 + Math.floor(Math.random() * 3)], canvas.width, 0, 2.5, 59, canvas.height))
 					next++;
 					objects[item].spawned = true;
 					score++;
@@ -146,38 +165,39 @@ window.addEventListener('load', function() {
 				objects[item].draw();
 			}
 		}
-		if(objects[next] && alive){
+		if (objects[next] && alive) {
 			var offset = (tick - objects[next].t) * objects[next].depth;
-			if(player.x + 60 > objects[next].x - offset && player.x + 60 < objects[next].x - offset + objects[next].w){
-				switch(objects[next].fill){
+			if (player.x + 60 > objects[next].x - offset && player.x + 60 < objects[next].x - offset + objects[next].w) {
+				switch (objects[next].fill) {
 					case imgs[8]:
-						if(player.y > 200)
+						if (player.y > 200)
 							died();
 						break;
 					case imgs[9]:
-						if(player.y  < 200 || player.y > 350)
+						if (player.y < 200 || player.y > 350)
 							died();
 						break;
 					case imgs[10]:
-						if(player.y  < 350)
+						if (player.y < 350)
 							died();
 						break;
-					default: break;
+					default:
+						break;
 				}
 			}
 		}
-		if(player.y < 0){
+		if (player.y < 0) {
 			player.y = 0;
 			velocity = gravity;
 		}
-		if(player.y + player.h >= canvas.height-68 && alive)
+		if (player.y + player.h >= canvas.height - 68 && alive)
 			died();
 		player.draw();
-		if(!alive){
-			if(within) objects[1].draw();
-			else  objects[0].draw();
+		if (!alive) {
+			if (within) objects[1].draw();
+			else objects[0].draw();
 		}
-		
+
 		context.font = "40px Sans-serif"
 		context.strokeStyle = 'black';
 		context.lineWidth = 8;
@@ -187,7 +207,7 @@ window.addEventListener('load', function() {
 		context.fillText('Best: ' + best, 10, 45);
 		context.fillText('Last: ' + last, 10, 90);
 		context.font = "85px Sans-serif";
-		context.strokeText(score, 10, canvas.height - 20);	
-		context.fillText(score, 10, canvas.height - 20);		
+		context.strokeText(score, 10, canvas.height - 20);
+		context.fillText(score, 10, canvas.height - 20);
 	})();
-}); 
+});
